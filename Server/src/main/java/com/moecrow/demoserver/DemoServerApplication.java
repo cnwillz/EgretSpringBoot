69fdea8f173b014e2f8ae76fb30abd288e89aedc
cnwillz <wdzzyx@126.com>
2020-10-29 15:15:23 +0800

    add server basic stomp

--- Server/src/main/java/com/moecrow/demoserver/DemoServerApplication.java
+++ Server/src/main/java/com/moecrow/demoserver/DemoServerApplication.java
@@ -2,7 +2,9 @@ package com.moecrow.demoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DemoServerApplication {
 
